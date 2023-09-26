2fad791318f115d177354e08ecda327ef3df020b
Iñaki Tuda <inakituda@gmail.com>
2023-09-26 08:19:18 -0300

    Cambios en el repo

--- PPI/src/dao/dbConfig.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const URI="mongodb+srv://inakituda:123456ituda@cluster0.pbsxdwh.mongodb.net/ecommerce?retryWrites=true&w=majority";
-
-await mongoose.connect(URI, {
-    serverSelectionTimeoutMS: 5000,
-});
-console.log("Conectado a la Base de Datos")
